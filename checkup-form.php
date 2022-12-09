f30d058721c00381482f291337fd649828783753
Laurence <sempailaurence@gmail.com>
2022-12-09 14:56:54 +0800

    workarea update

--- checkup-form.php
+++ checkup-form.php
@@ -21,11 +21,23 @@ include("layout-header.php") ;
                 <?php include("layouts/navigation.php") ?>
 
                 <!--WORKAREA-->
-                <section class="workarea w-100">
+                <section class="workarea w-100 pb-5">
+
+                    <!--WELCOME BANNER-->
                     <div class="bg-secondary welcome-banner bg-control d-flex align-items-center px-2 fw-bold fs-6">
                         <span class="welcome-text me-auto">Welcome, user!</span>
                         <span class="date-banner"><?php echo Dates::dateToday("l, F d, Y"); ?></span>
                     </div>
+
+                    <!--THE CHECKUP FORM-->
+                    <div class="checkup-form-wrapper p-4 w-100 h-100">
+
+                        <div class="checkup-form bg-white shadow-2-strong w-100 h-100">
+
+                        </div>
+
+                    </div>
+
                 </section>
 
             </section>
